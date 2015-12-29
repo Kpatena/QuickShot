50507dc2eed8940f0c15bf5ced1b731029a3671e
Kpatena <kamilpatena1221@gmail.com>
2015-12-28 22:04:59 -0800

    Layout

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+	public float speed;
+	public float rotationSpeed;
+
+	public Transform player;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float step = speed * Time.deltaTime;
+		transform.Rotate (new Vector3 (15, 30, 45) * (rotationSpeed * Time.deltaTime));
+		transform.position = Vector3.MoveTowards (transform.position, player.position, step);
+	}
+}
